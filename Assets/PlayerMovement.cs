7e142559d12c541887c8309b8a36fba2fe82432b
suzaankhan <92640978+suzaankhan@users.noreply.github.com>
2024-12-19 01:31:37 +0530

    Update PlayerMovement.cs

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,27 +1,4 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
 
-// public class playerScript : MonoBehaviour
-// {
-//     public Rigidbody rb;
-    
-//     public float forwardForce = 2000f;
-//     public float movementForce = 700f;
-
-//     // Update is called once per frame
-//     void FixedUpdate()
-//     {
-//         rb.AddForce(0,0,forwardForce * Time.deltaTime);
-
-//         if(Input.GetKey("d")){
-//             rb.AddForce(movementForce * Time.deltaTime, 0,0, ForceMode.VelocityChange);
-//         }
-//         if(Input.GetKey("a")){
-//             rb.AddForce(-movementForce * Time.deltaTime, 0,0, ForceMode.VelocityChange);
-//         }
-//     }
-// }
 
 using UnityEngine.UI;
 using UnityEngine;
